90abd23ddfc711af6dbd2ef182b77f5ed2177866
hanifabdihakimhani <115537171+hanifabdihakimhani@users.noreply.github.com>
2023-01-13 20:20:22 +0700

    Add basic calculator functionality Add operator functions

---
+++ script.js
@@ -0,0 +1,17 @@
+const add = function (a, b) { return a + b };
+const subtract = function (a, b) { return a - b };
+const multiply = function (a, b) { return a * b };
+const divide = function (a, b) { return a / b };
+
+const operate = function (operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
