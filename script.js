747fae3a459e12e3a96edd60f051401bd8b89ed8
hanifabdihakimhani <115537171+hanifabdihakimhani@users.noreply.github.com>
2023-01-14 20:44:12 +0700

    Make calculator layout Display number on calculator user input button

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const add = function (a, b) { return a + b };
-const subtract = function (a, b) { return a - b };
-const multiply = function (a, b) { return a * b };
-const divide = function (a, b) { return a / b };
+const numButton = document.querySelectorAll('[data-number]');
+const operatorButton = document.querySelectorAll('[data-operation]');
+const display = document.getElementById('display-value');
+const equalsButton = document.querySelector('[data-equals]');
+let result = "";
 
-const operate = function (operator, a, b) {
+function add(a, b) { return a + b };
+function subtract(a, b) { return a - b };
+function multiply(a, b) { return a * b };
+function divide(a, b) { return a / b };
+
+function operate(operator, a, b) {
     switch (operator) {
         case '+':
             return add(a, b);
@@ -15,3 +21,27 @@ const operate = function (operator, a, b) {
             return divide(a, b);
     }
 }
+
+
+numButton.forEach(button => {
+    button.addEventListener('click', function (e) {
+        number = e.target.dataset.number;
+        result += number;
+        display.value = result;
+    })
+})
+
+function displayValue() {
+    let number = numInput();
+    display.textContent = number;
+}
+
+operatorButton.forEach(button => {
+    button.addEventListener('click', function (e) {
+        operator = e.target.dataset.operation;
+    })
+})
+
+equalsButton.addEventListener('click', function (e) {
+    result = operate(operator, a, b);
+})
